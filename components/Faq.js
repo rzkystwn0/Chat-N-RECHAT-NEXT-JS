aedfc3f8138f4bbb521f8b8de3a24c8657f400f8
rzkystwn0 <rzkystwn0@gmail.com>
2022-04-20 16:07:41 +0700

    edit faq

--- components/Faq.js
+++ components/Faq.js
@@ -7,9 +7,9 @@ export default function Faq() {
     <Container>
       <div className="py-32">
         <div className="w-12/12 text-center py-18 " id="faq">
-          <p className="SourceSans text-sm font-semibold tracking-widest mb-4">NGGAK HARUS SIH SEBENERNYA</p>
-          <p className="DM-Sans text-2xl w-64 mb:w-0 mx-auto tracking-wide text-blue-900 mb-4">Kenapa Harus Pakai Chat N’ Rechat?</p>
-          <p className="tracking-wide text-black/60 SourceSans text-base md:w-4/12 mx-auto">Sebenernya yang bagus itu sih WhatsApp loh, mending pake WhatsApp aja deh. Yaudah berikut ini adalah fitur-fitur WhatsApp.</p>
+          <p className="SourceSans text-sm font-semibold tracking-widest mb-4">BANYAK YANG NANYA</p>
+          <p className="DM-Sans text-2xl w-64 mb:w-0 mx-auto tracking-wide text-blue-900 mb-4">Pertanyaan Yang Sering Ditanyain</p>
+          <p className="tracking-wide text-black/60 SourceSans text-base md:w-4/12 mx-auto">Sebenernya pertanyaan-pertanyaan ini kagak ada yang nanya sih, tapi ya ditaruh sini aja siapa tau lo mau baca ‘kan.</p>
         </div>
         <div className="md:w-8/12 w-11/12 mx-auto ">
         <FaqPacking />
